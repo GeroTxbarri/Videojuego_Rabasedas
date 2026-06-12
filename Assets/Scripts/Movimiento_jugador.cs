3132c479b9c89b5c372698820fe190a580ae0864
Manuel <manuferrerpetit@gmail.com>
2026-06-12 17:20:05 -0300

    intento de animator

--- Assets/Scripts/Movimiento_jugador.cs
+++ Assets/Scripts/Movimiento_jugador.cs
@@ -102,6 +102,14 @@ public class Movimiento_jugador : MonoBehaviour
         float movHorizontal = Input.GetAxisRaw("Horizontal");
         float movVertical   = Input.GetAxisRaw("Vertical");
 
+        anim.SetFloat("Yspeed",movVertical);
+        anim.SetFloat("Xspeed",movHorizontal);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("IsGrounded");
+        }
+
         Vector3 direccion = (transform.right * movHorizontal + transform.forward * movVertical).normalized;
 
         if (modoCargar && tocaPiso)
@@ -135,13 +143,7 @@ public class Movimiento_jugador : MonoBehaviour
 
     void Update()
     {
-        anim.SetFloat("YSpeed",Input.GetAxis("Vertical"));
-        anim.SetFloat("XSpeed",Input.GetAxis("Horizontal"));
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetTrigger("IsGrounded");
-        }
+        
         // 1. La detección de piso SIEMPRE debe ejecutarse, incluso estando paralizado
         Vector3 origen = transform.position + Vector3.down * offsetSuelo;
         Collider[] colliders = Physics.OverlapSphere(origen, radioDeteccion, capasPiso, QueryTriggerInteraction.Ignore);
